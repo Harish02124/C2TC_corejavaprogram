ee4ccfed7fceb5f01b8aa0936cbe4111f972e9e3
Harish <Harish@DESKTOP-QPV841I>
2025-08-01 09:59:12 +0530

    new

---
+++ C2TC_corejavaprograms/src/Assignment_6/InsufficientFundsException.java
@@ -0,0 +1,8 @@
+package Assignment_6;
+
+//Custom exception for insufficient funds during withdrawal
+public class InsufficientFundsException extends Exception {
+ public InsufficientFundsException(String message) {
+     super(message);
+ }
+}
---
+++ C2TC_corejavaprograms/src/Assignment_6/InvalidAmountException.java
@@ -0,0 +1,9 @@
+package Assignment_6;
+
+//Custom exception for invalid amounts in deposit or withdrawal
+public class InvalidAmountException extends Exception {
+ public InvalidAmountException(String message) {
+     super(message);
+ }
+}
+
